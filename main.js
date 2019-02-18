7dcadd95500f016c9b37e466128478ef0c1aed9c
Iana-Neri22 <iananeri@outlook.com>
2019-02-18 15:32:08 -0300

    event listener adicionado no botao

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
-function twittar(){
+/*function twittar(){
 let tweet = document.getElementById("tweet").value;
 document.getElementById("myTweet").innerHTML = tweet;
-}
+}*/
+let botaoEnviar = document.getElementById("sendTweet")
+function twittar(){
+    let tweet = document.getElementById("tweet").value;
+    document.getElementById("myTweet").innerHTML = tweet;
+    }
+botaoEnviar.addEventListener("click", twittar);

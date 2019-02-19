217f327574259a0e05a86f6e596fbaf86f8e432b
Iana-Neri22 <iananeri@outlook.com>
2019-02-18 21:08:47 -0300

    event listener testado e funcionando

--- main.js
+++ main.js
@@ -1,10 +1,9 @@
 /*Linha de código que desativa o botão enviar*/ 
 document.getElementById('sendTweet').disabled = true;
 
-function twittar(){
-    let tweet = document.getElementById("tweet").value;
-    document.getElementById("myTweet").innerHTML = tweet;
-    }
+let tweet = document.getElementById("tweet");
+tweet.addEventListener("keyup", typing);
+tweet.addEventListener("keyup", charcountupdate);
 
 function typing(){
     let texto = document.getElementById("tweet").value;
@@ -14,13 +13,16 @@ function typing(){
             document.getElementById('sendTweet').disabled = true;
         }
 }
-/*A cor não limpa sem o refresh, arrumar isso*/
-/*Talvez usar o event listener ajude?*/ 
+
 function charcountupdate() {
     let str = document.getElementById("tweet").value;
     let characters = 140;
       let lng = characters - str.length;
       document.getElementById("contador").innerHTML = lng;
+
+      if(str.length <120){
+          contador.style.color = "#000";
+      }
 
       if(str.length >=120 && str.length<=129){
         contador.style.color = '#ffa500';
@@ -30,46 +32,15 @@ function charcountupdate() {
         contador.style.color = '#ff0000';
       }
 
-      if(str.length >= 140){
+      if(str.length > 140){
         document.getElementById('sendTweet').disabled = true;
       }
   }
 
-/*let botaoEnviar = document.getElementById("sendTweet")
-function twittar(){
+let sendTweet= document.getElementById("sendTweet");
+sendTweet.addEventListener("click", twittar);
+
+  function twittar(){
     let tweet = document.getElementById("tweet").value;
     document.getElementById("myTweet").innerHTML = tweet;
     }
-botaoEnviar.addEventListener("click", twittar);
-
-
-
-function stoppedTyping(){
-    let texto = getElementById("tweet").value;
-
-    if(texto.value.length > 0) { 
-        document.getElementById('sendTweet').disabled = false; 
-    } else { 
-        document.getElementById('sendTweet').disabled = true;
-    }
-}
-
-
-
-
-//document.getElementById('sendTweet').disabled = true;
-
-/*function stoppedTyping(){
-    if(this.value.length > 0) { 
-        document.getElementById('sendTweet').disabled = false; 
-    } else { 
-        document.getElementById('sendTweet').disabled = true;
-    }
-}
-function verify(){
-    if (tweet === null){
-        alert("Put some text in there!")
-        return
-    }
-    
-}*/

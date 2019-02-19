0daf52d1882518d2367cc209402303007860df5f
Iana-Neri22 <iananeri@outlook.com>
2019-02-19 13:00:28 -0300

    ~corrigi o erro do botão que não desativava

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 /*Linha de código que desativa o botão enviar*/ 
 document.getElementById('sendTweet').disabled = true;
 const typing = () => {
-    let texto = document.getElementById("tweet").value;
-        if(texto.length > 0) { 
+    const text = document.getElementById("tweet").value;
+        if(text.length > 0 && text.length <=140) { 
             document.getElementById('sendTweet').disabled = false; 
-        } else { 
-            document.getElementById('sendTweet').disabled = true;
+        } 
+        else{
+          document.getElementById('sendTweet').disabled = true;
         }
+
 }
 
 const counter = () => {
-    let str = document.getElementById("tweet").value;
-    let characters = 140;
-      let lng = characters - str.length;
+    const str = document.getElementById("tweet").value;
+      let lng =  140 - str.length;
       document.getElementById("contador").innerHTML = lng;
 
       if(str.length <120){
@@ -25,11 +26,7 @@ const counter = () => {
 
       if(str.length >=130){
         contador.style.color = '#ff0000';
-      }
-
-      if(str.length > 140){
-        document.getElementById('sendTweet').disabled = true;
-      }
+      } 
   }
 
 let tweet = document.getElementById("tweet");
@@ -44,6 +41,7 @@ const twittar = () =>{
 const clean = () =>{
     document.getElementById("tweet").value = " ";
     document.getElementById('sendTweet').disabled = true;
+    document.getElementById("contador").innerHTML = 140;
 }
 
 let sendTweet= document.getElementById("sendTweet");

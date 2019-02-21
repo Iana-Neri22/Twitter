576a0ff9e437a803d50edd457318e9ec2443f85f
Iana-Neri22 <iananeri@outlook.com>
2019-02-21 13:59:11 -0300

    limpando o código

--- src/twitter.js
+++ src/twitter.js
@@ -1,87 +1,82 @@
-const disableButton = () => document.querySelector('#sendTweet').disabled = true;
+const disableButton = () =>
+  (document.querySelector("#sendTweet").disabled = true);
 window.addEventListener("load", disableButton);
 
 const typing = () => {
-    const text = document.getElementById("tweet").value;
-    const button = document.querySelector('#sendTweet')
-        if(text.length > 0 && text.length <=140) { 
-            button.disabled = false; 
-        } 
-        else{
-          button.disabled = true; 
-        }
-}
+  const text = document.getElementById("tweet").value;
+  const button = document.querySelector("#sendTweet");
+  if (text.length > 0 && text.length <= 140) {
+    button.disabled = false;
+  } else {
+    button.disabled = true;
+  }
+};
 
 const counter = () => {
-    const str = document.getElementById("tweet").value.length;
-    const lng =  140 - str;
-      document.getElementById("contador").innerHTML = lng;
+  const str = document.getElementById("tweet").value.length;
+  const lng = 140 - str;
+  document.getElementById("contador").innerHTML = lng;
 
-      if(str >=120 && str<=129){
-        contador.style.color = '#ffa500';
-      }
-
-      else if(str >=130){
-        contador.style.color = '#ff0000';
-      } 
-
-      else{
-        contador.style.color = "#008000"
-      }
+  if (str >= 120 && str <= 129) {
+    contador.style.color = "#ffa500";
+  } else if (str >= 130) {
+    contador.style.color = "#ff0000";
+  } else {
+    contador.style.color = "#008000";
   }
+};
 
-  const autoResize = () =>{
-    const textArea = document.getElementById('tweet');
-    while (textArea.scrollHeight > textArea.offsetHeight)
-    {
-        textArea.rows += 1;
-    }
-}
+const autoResize = () => {
+  const textArea = document.getElementById("tweet");
+  while (textArea.scrollHeight > textArea.offsetHeight) {
+    textArea.rows += 1;
+  }
+};
 
-const tweet = document.querySelector('#tweet');
+const tweet = document.querySelector("#tweet");
 tweet.addEventListener("keyup", typing);
 tweet.addEventListener("keyup", counter);
 tweet.addEventListener("keydown", autoResize);
 
-const clean = () =>{
-    document.getElementById("tweet").value = " ";
-    document.getElementById('sendTweet').disabled = true;
-    document.getElementById("contador").innerHTML = 140;
-    contador.style.color = "#008000"
-}
+const clean = () => {
+  document.getElementById("tweet").value = " ";
+  document.getElementById("sendTweet").disabled = true;
+  document.getElementById("contador").innerHTML = 140;
+  contador.style.color = "#008000";
+};
 
-const horario = () =>{
-    let data = new Date ();
-    let horas = data.getHours ();
-    let minutos = data.getMinutes ();
+const horario = () => {
+  let data = new Date();
+  let horas = data.getHours();
+  let minutos = data.getMinutes();
 
-    if (minutos < 10){
-        minutos = "0" + minutos;
-    }
+  if (minutos < 10) {
+    minutos = "0" + minutos;
+  }
 
-    if (horas < 10){
-        horas = "0" + horas;
-    }
+  if (horas < 10) {
+    horas = "0" + horas;
+  }
 
-    let horaTweet = horas + ":" + minutos
-    return horaTweet;
-}
+  let horaTweet = horas + ":" + minutos;
+  return horaTweet;
+};
 
-const twittar = () =>{
-    const tweet = document.getElementById("tweet").value;
-    return tweet
-    }
+const twittar = () => {
+  const tweet = document.getElementById("tweet").value;
+  return tweet;
+};
 
-const printarTweet = () =>{
-    let lista = document.getElementById("aparecerTweets")
-    
-    let campo = document.createElement("textarea");
-    campo.setAttribute("class", "txtAreaTweet");
-    campo.textContent = horario() + " " + twittar()
-        
-      
-    lista.insertBefore(campo, lista.childNodes[0])}
-        
-let sendTweet= document.getElementById("sendTweet");
-sendTweet.addEventListener("click", printarTweet);
+const printTweet = () => {
+  let lista = document.getElementById("showTweets");
+
+  let campo = document.createElement("textarea");
+  campo.setAttribute("class", "txtAreaTweet");
+  campo.textContent = horario() + " " + twittar();
+
+  lista.insertBefore(campo, lista.childNodes[0]);
+};
+
+const sendTweet = document.getElementById("sendTweet");
+sendTweet.addEventListener("click", printTweet);
 sendTweet.addEventListener("click", clean);
